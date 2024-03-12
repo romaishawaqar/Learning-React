3168f139a371f2b065c9a810eeca22b35031cd24
Vivek Kumar <contact.vivek1009@gmail.com>
2024-03-12 20:39:59 +0530

    Used Tailwind and Props

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -1,24 +1,34 @@
 import { useState } from "react";
+import Card from "./Card";
 
 function App() {
 
   const [Counter, setCounter] = useState(0);
 
-  function Clickedp(){
-    setCounter(Counter+1);
-    if(Counter > 20) setCounter(0);
-  }
-  function Clickedn(){
-    setCounter(Counter-1);
-    if(Counter < 0) setCounter(20);
-  }
+  // function Clickedp(){
+  //   setCounter(Counter+1);
+  //   if(Counter > 20) setCounter(0);
+  // }
+  // function Clickedn(){
+  //   setCounter(Counter-1);
+  //   if(Counter < 0) setCounter(20);
+  // }
+  let users = ['Vivek', 'Romaisha', 'Anshu', 'Mummy', 'Papa', 'Dadi'];
   
   return (
     <>
-    <h1>Vivek is learning react with vite</h1>
-    <h1>Counter Value: {Counter}</h1>
-    <button onClick={Clickedp}>+</button>
-    <button onClick={Clickedn}>-</button>
+     <h1 className="bg-green-400 text-black ">Tailwind Css use</h1>
+     <h1>Vivek</h1>
+     <div className="flex flex-row-reverse p-2">
+        <Card name = {users[0]} />
+        <Card name = {users[1]} />
+        <Card name = {users[2]} />
+     </div>
+     <div class="flex flex-wrap">
+        <Card name = {users[3]} />
+        <Card name = {users[4]} />
+        <Card name = {users[5]} />
+     </div>
     </>
   )
 }
--- ReactApp/src/Names.jsx
+++ ReactApp/src/Names.jsx
@@ -1,6 +1,8 @@
 function Name(){
     return(
+        <>
         <h1>My name is Vivek</h1>
+        </>
     )
 }
 
