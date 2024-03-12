5b086700b2a8e1f553b2603b4119e473f7cf4fd6
Vivek Kumar <contact.vivek1009@gmail.com>
2024-03-12 17:45:18 +0530

    Made counter

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 function App() {
+
+  const [Counter, setCounter] = useState(0);
+
+  function Clickedp(){
+    setCounter(Counter+1);
+    if(Counter > 20) setCounter(0);
+  }
+  function Clickedn(){
+    setCounter(Counter-1);
+    if(Counter < 0) setCounter(20);
+  }
   
   return (
+    <>
     <h1>Vivek is learning react with vite</h1>
+    <h1>Counter Value: {Counter}</h1>
+    <button onClick={Clickedp}>+</button>
+    <button onClick={Clickedn}>-</button>
+    </>
   )
 }
 
