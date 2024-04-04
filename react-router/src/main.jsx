3a9e2fb2bace659783894c34a20af45cc9d616e2
Vivek Kumar <contact.vivek1009@gmail.com>
2024-04-04 12:23:39 +0530

    Router things

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
 
 const router = createBrowserRouter([
   {
-    path: '/'
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: '',
+        element: <Home/>
+      }, 
+      {
+        path: 'about',
+        element: <About/>
+      }
+    ]
   }
 ]);
 
