affb3bb24c5236c9a7edf306da33549a01d635bc
Vivek Kumar <contact.vivek1009@gmail.com>
2024-05-10 11:40:29 +0530

    config setting

---
+++ Megablog/src/appwrite/config.js
@@ -0,0 +1,123 @@
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+import conf from "../conf/conf";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollection_Id,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+    }
+  }
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollection_Id,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+    }
+  }
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollection_Id,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+      return false;
+    }
+  }
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollection_Id,
+        slug
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+      return false;
+    }
+  }
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollection_Id,
+        queries
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+      return false;
+    }
+  }
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique,
+        file
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+      return false;
+    }
+  }
+  async deleteFile(fileId) {
+    try {
+       await this.bucket.deleteFile(
+        conf.appwriteBucketId,
+        fileId
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+      return false;
+    }
+  }
+  getFilePreview(fileId){
+    return this.bucket.getFilePreview(
+        conf.appwriteBucketId,
+        fileId
+    )
+  }
+}
+
+const service = new Service()
+
+export default service
+
